0373e31191e51beea143b6c065a6911cd03ef67e
Dan Rome <dan.rome@dardanup.wa.gov.au>
2018-08-14 04:50:12 +0100

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header class="banner">
+  <div class="container">
+    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light nav-primary">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu($primarymenu) !!}
+      @endif
+    </nav>
+  </div>
+</header>
