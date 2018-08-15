5f5f22b53e8fc6067782133678fd3ccfb2e7fbf8
Dan Rome <dan.rome@dardanup.wa.gov.au>
2018-08-15 08:51:34 +0100

    added screenshop.png

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="banner">
   <div class="container-fluid">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="navbar navbar-light bg-light nav-primary">
+    <nav class="navbar navbar-light bg-light">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
       </button>
